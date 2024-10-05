34cf9ca95a18a50f296b91fd49e1745c3f795441
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-10-05 18:47:40 -0400

    added homepage

---
+++ communitycare_frontend/src/components/JumboTron.jsx
@@ -0,0 +1,7 @@
+const JumboTron = () => {
+    return(
+        <div>
+            <h1>Connecting Communities with Relief</h1>
+            <p>CommunityCare connect communities impacted by natural disasters with vital relief resources, volunteers, and first responders, facilitating swift rescue efforts and supporting long-term rebuilding.</p>
+        </div>
+    )
