e5a5d767269738301268e1aa3abc23481c7fa880
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-10-05 22:29:48 -0400

    Added

--- communitycare_frontend/src/App.js
+++ communitycare_frontend/src/App.js
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-
+import RequestHelp from './components/RequestHelp';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        
+        <Route path="/request-help" element={<RequestHelp />} />
       </Routes>
     </Router>
   );
