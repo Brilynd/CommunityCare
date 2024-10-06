a9a63887569632b475a53e2f2c95d2e08bf829d1
Brilynd <86674962+Brilynd@users.noreply.github.com>
2024-10-06 00:49:48 -0400

    Updated to enable signin signout

--- communitycare_frontend/src/components/Navbar.jsx
+++ communitycare_frontend/src/components/Navbar.jsx
@@ -1,17 +1,22 @@
-// Navbar.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { HeartHandshake, CircleX, Menu } from 'lucide-react';
+import { useCookies } from 'react-cookie'; // Import useCookies
 
 import './Navbar.css'; // Import the CSS file
 
-const Navbar = ({ toggleSignUpPopup, toggleSignInPopup,currSelected }) => {
+const Navbar = ({ toggleSignUpPopup, toggleSignInPopup, currSelected }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  const [cookies, setCookie, removeCookie] = useCookies(['user']); // Initialize cookies
+  const isLoggedIn = !!cookies.user; // Check if user cookie exists
+  
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  const handleSignOut = () => {
+    removeCookie('user'); // Remove user cookie
+      };
 
   return (
     <div className="navbar-container">
@@ -25,10 +30,10 @@ const Navbar = ({ toggleSignUpPopup, toggleSignInPopup,currSelected }) => {
           {isMobileMenuOpen ? <CircleX size="30" /> : <Menu size="30" />}
         </button>
         <div className={`links ${isMobileMenuOpen ? 'show' : ''}`}>
-          <Link to="/" className={currSelected=="Home"?"link nav-link active":"link nav-link"} onClick={toggleMobileMenu}>
+          <Link to="/" className={currSelected === "Home" ? "link nav-link active" : "link nav-link"} onClick={toggleMobileMenu}>
             Home
           </Link>
-          <Link to="/request-help" className={currSelected=="RequestHelp"?"link nav-link active":"link nav-link"} onClick={toggleMobileMenu}>
+          <Link to="/request-help" className={currSelected === "RequestHelp" ? "link nav-link active" : "link nav-link"} onClick={toggleMobileMenu}>
             Request Help
           </Link>
           <Link to="/view-requests" className="link nav-link" onClick={toggleMobileMenu}>
@@ -38,11 +43,11 @@ const Navbar = ({ toggleSignUpPopup, toggleSignInPopup,currSelected }) => {
             Profile
           </Link>
           {isLoggedIn ? (
-            <p className="link nav-link" onClick={toggleSignUpPopup}>
+            <p className="link nav-link" onClick={handleSignOut}>
               Sign Out
             </p>
           ) : (
-            <p className="link nav-link" onClick={() => toggleSignInPopup()}>
+            <p className="link nav-link" onClick={toggleSignInPopup}>
               Sign In
             </p>
           )}
