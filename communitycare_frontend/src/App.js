cb4bce04254e2fba2f4d91d5b816411c34c49e72
moniquejohnsonn <101659813+moniquejohnsonn@users.noreply.github.com>
2024-10-05 22:47:58 -0400

    Revert "fixing navbar to be on all pages"
    This reverts commit d7aa0777acb9b084f9406163c531456e2bcd5363.

--- communitycare_frontend/src/App.js
+++ communitycare_frontend/src/App.js
@@ -1,39 +1,18 @@
+// App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import RequestHelp from './components/RequestHelp';
-import Navbar from './components/Navbar';
-import SignUpPopup from './components/SignUp';
-import SignIn from './components/SignIn';
-
-const App = () => {
-    const [signUpPopup, setSignUp] = useState(false);
-    const [signInPopup, setSignIn] = useState(false);
-
-    // Toggle SignIn and SignUp functions
-    const toggleSignInPopup = () => {
-        setSignIn(!signInPopup);
-    };
-
-    const toggleSignUpPopup = () => {
-        setSignUp(!signUpPopup);
-    };
-
-    return (
-        <Router>
-            <Navbar toggleSignUpPopup={toggleSignUpPopup} toggleSignInPopup={toggleSignInPopup} />
-
-            {/* Define all your routes here */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/request-help" element={<RequestHelp />} />
-                {/*<Route path="/view-requests" element={<ViewRequests />} />
-                <Route path="/profile" element={<Profile />} /> */}
-            </Routes>
-            {signUpPopup && <SignUpPopup toggleSignUpPopup={toggleSignUpPopup} />}
-            {signInPopup && <SignIn toggleSignInPopup={toggleSignInPopup} />}
-        </Router>
-    );
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/request-help" element={<RequestHelp />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
