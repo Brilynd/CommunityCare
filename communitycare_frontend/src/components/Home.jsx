8ed641a9f818fedcf582d95c4125ac4eb67701d0
moniquejohnsonn <101659813+moniquejohnsonn@users.noreply.github.com>
2024-10-05 23:43:24 -0400

    adding toggleSignUp to be passed from home to signIn

--- communitycare_frontend/src/components/Home.jsx
+++ communitycare_frontend/src/components/Home.jsx
@@ -1,30 +1,35 @@
-import "./Home.css"
-import JumboTron from "./JumboTron"
-import SignupPopup from "./SignUp"
-import SignIn from "./SignIn"
-import Navbar from "./Navbar"
-import SupportUs from "./SupportUs"
-import {useState} from "react"
-import InformationDisplay from "./InformationDisplay"
-const Home = () =>{
-const [signUpPopup, setSignUp] = useState(false)
-const [signInPopup, setSignIn] = useState(false)
-const [isSignedIn, setIsSignedIn] = useState(false)
-const toggleSignInPopup = () => {
-    setSignIn(!signInPopup)
-}
-const toggleSignUpPopup = () => {
-    setSignUp(!signUpPopup)
-}
-return(
-    <div>
-        <Navbar toggleSignUpPopup={toggleSignUpPopup} toggleSignInPopup={toggleSignInPopup} currSelected={"Home"}/>
-        <JumboTron toggleSignIn={toggleSignInPopup} isSignedIn={isSignedIn}/>
-        <SupportUs/>
-        <InformationDisplay/>
-        {signUpPopup && <SignupPopup toggleSignUpPopup={toggleSignUpPopup}/>}
-        {signInPopup && <SignIn toggleSignInPopup={toggleSignInPopup}/>}
-    </div>
-)
-}
-export default Home
+import "./Home.css";
+import JumboTron from "./JumboTron";
+import SignupPopup from "./SignUp";
+import SignIn from "./SignIn";
+import Navbar from "./Navbar";
+import SupportUs from "./SupportUs";
+import { useState } from "react";
+import InformationDisplay from "./InformationDisplay";
+
+const Home = () => {
+    const [signUpPopup, setSignUp] = useState(false);
+    const [signInPopup, setSignIn] = useState(false);
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    const toggleSignInPopup = () => {
+        setSignIn(!signInPopup);
+    };
+
+    const toggleSignUpPopup = () => {
+        setSignUp(!signUpPopup);
+    };
+
+    return (
+        <div>
+            <Navbar toggleSignUpPopup={toggleSignUpPopup} toggleSignInPopup={toggleSignInPopup} currSelected={"Home"}/>
+            <JumboTron toggleSignIn={toggleSignInPopup} isSignedIn={isSignedIn} />
+            <SupportUs />
+            <InformationDisplay />
+            {signUpPopup && <SignupPopup toggleSignUpPopup={toggleSignUpPopup} />}
+            {signInPopup && (<SignIn toggleSignInPopup={toggleSignInPopup} toggleSignUpPopup={toggleSignUpPopup}/>)}
+        </div>
+    );
+};
+
+export default Home;
