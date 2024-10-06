d7aa0777acb9b084f9406163c531456e2bcd5363
moniquejohnsonn <101659813+moniquejohnsonn@users.noreply.github.com>
2024-10-05 22:45:56 -0400

    fixing navbar to be on all pages

--- communitycare_frontend/src/components/Home.jsx
+++ communitycare_frontend/src/components/Home.jsx
@@ -1,28 +1,15 @@
-import "./Home.css"
-import JumboTron from "./JumboTron"
-import SignupPopup from "./SignUp"
-import SignIn from "./SignIn"
-import Navbar from "./Navbar"
-import SupportUs from "./SupportUs"
-import {useState} from "react"
-const Home = () =>{
-const [signUpPopup, setSignUp] = useState(false)
-const [signInPopup, setSignIn] = useState(false)
-const [isSignedIn, setIsSignedIn] = useState(false)
-const toggleSignInPopup = () => {
-    setSignIn(!signInPopup)
-}
-const toggleSignUpPopup = () => {
-    setSignUp(!signUpPopup)
-}
-return(
-    <div>
-        <Navbar toggleSignUpPopup={toggleSignUpPopup} toggleSignInPopup={toggleSignInPopup} currSelected={"Home"}/>
-        <JumboTron toggleSignIn={toggleSignInPopup} isSignedIn={isSignedIn}/>
-        <SupportUs/>
-        {signUpPopup && <SignupPopup toggleSignUpPopup={toggleSignUpPopup}/>}
-        {signInPopup && <SignIn toggleSignInPopup={toggleSignInPopup}/>}
-    </div>
-)
-}
-export default Home
+import './Home.css';
+import JumboTron from './JumboTron';
+import SupportUs from './SupportUs';
+
+const Home = ({ toggleSignInPopup, isSignedIn }) => {
+    return (
+        <div>
+            {/* Pass the toggleSignInPopup and isSignedIn props to JumboTron */}
+            <JumboTron toggleSignIn={toggleSignInPopup} isSignedIn={isSignedIn} />
+            <SupportUs />
+        </div>
+    );
+};
+
+export default Home;
